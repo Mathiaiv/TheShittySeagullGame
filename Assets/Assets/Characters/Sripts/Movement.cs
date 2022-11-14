e4f475671a8a9fe44ac0e572c2d1b506235bfeb0
MrMative <iversenmathias95@gmail.com>
2022-11-14 18:54:01 +0100

    movement
    Added initial movement

---
+++ Assets/Assets/Characters/Sripts/Movement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    public float minSpeed = 1f;
+
+    Vector2 movementInput;
+    Rigidbody2D rb;
+    Vector2 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movementInput * minSpeed * Time.fixedDeltaTime);
+    }
+
+    /**
+    * This is the method that runs when reacieving movement input.
+    *
+    * @param movementInput is the movement input recived. 
+    */
+    void OnMove(InputValue movmentValue)
+    {
+        movementInput = movmentValue.Get<Vector2>();
+    }
+}
