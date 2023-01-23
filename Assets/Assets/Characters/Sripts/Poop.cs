61db29b3e950c2ee89d463065755b8b7a5c734a7
MrMative <iversenmathias95@gmail.com>
2023-01-23 19:12:10 +0100

    added poop bar
    made poop calculations and poop bar

--- Assets/Assets/Characters/Sripts/Poop.cs
+++ Assets/Assets/Characters/Sripts/Poop.cs
@@ -1,25 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Poop : MonoBehaviour
 {
     public GameObject poop;
+    public Slider poopBar;
+    public float maxPoop = 10;
+    public float poopRegenRate;
+    public float poopAmount;
         
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    /*
+    increases the poop gradualy over time
+    */
+    void FixedUpdate()
     {
-        
+        if (poopAmount < maxPoop) 
+        {
+            poopAmount += poopRegenRate * Time.fixedDeltaTime;
+        }
+        else poopAmount = 10;
+        poopBar.value = poopAmount;
     }
 
+    /*
+    The seagull poops if there is poop inside your body
+    */
     void OnFire()
     {
-        GameObject.Instantiate(poop, transform.position, default);
+        if ( poopAmount > 1) 
+        {
+            poopAmount--;
+            GameObject.Instantiate(poop, transform.position, default);
+        }
     }
 }
