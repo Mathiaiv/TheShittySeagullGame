69057ba18c67d63ac782ba0c1deb69f1e0db1ce8
MrMative <iversenmathias95@gmail.com>
2023-02-06 00:09:15 +0100

    cleaned up a bit an made prefabs and wrote some comments

--- Assets/Assets/Characters/Person/PersonMovement.cs
+++ Assets/Assets/Characters/Person/PersonMovement.cs
@@ -1,23 +1,46 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A script handling the movement of a person
+/// </summary>
 public class PersonMovement : MonoBehaviour
 {
     public Transform start;
     public Transform end;
-    public float speed;
+    [SerializeField] private float speed = 1f;
 
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 _velocity;
+    
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    private void Start()
     {
-        this.transform.position = start.position;
+        var startPos = start.position;
+        transform.position = startPos;
+        _velocity = (end.position - startPos).normalized * speed;
     }
-
-    // Update is called once per frame
+    
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
     private void Update()
     {
-        transform.position = Vector2.Lerp(transform.position, end.position, Time.deltaTime);
+        if (!IsInRangeOfPoint(end.position, 0.5f)) transform.position += _velocity * Time.fixedDeltaTime;
+    }
+
+    /// <summary>
+    /// Checks if this person is in range of a 2d point
+    /// </summary>
+    /// <param name="point"> the pont ot check against </param>
+    /// <param name="range"> the range in both x and y axis from the point </param>
+    /// <returns> true if the person is in range of false if not</returns>
+    private bool IsInRangeOfPoint (Vector2 point, float range)
+    {
+        Vector2 pos = transform.position;
+        return pos.x > point.x + range 
+               && pos.x < point.x - range 
+               && pos.y > point.y + range 
+               && pos.y < point.y - range;
     }
 }
