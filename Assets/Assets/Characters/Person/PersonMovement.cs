b551a7e1890c546ff2f8d04546bea23bfe66b73c
MrMative <iversenmathias95@gmail.com>
2023-02-06 20:30:44 +0100

    made changes to person spawner and person movement

--- Assets/Assets/Characters/Person/PersonMovement.cs
+++ Assets/Assets/Characters/Person/PersonMovement.cs
@@ -7,18 +7,16 @@ public class PersonMovement : MonoBehaviour
 {
     public Transform start;
     public Transform end;
+    public bool isFinished;
     [SerializeField] private float speed = 1f;
-
+    [SerializeField] private float finishRadius = 1f;
     private Vector3 _velocity;
     
-    /// <summary>
-    /// Start is called before the first frame update
-    /// </summary>
-    private void Start()
+    public void Spawn()
     {
-        var startPos = start.position;
-        transform.position = startPos;
-        _velocity = (end.position - startPos).normalized * speed;
+        transform.position = start.position;
+        UpdateVelocity();
+        isFinished = false;
     }
     
     /// <summary>
@@ -26,21 +24,19 @@ public class PersonMovement : MonoBehaviour
     /// </summary>
     private void Update()
     {
-        if (!IsInRangeOfPoint(end.position, 0.5f)) transform.position += _velocity * Time.fixedDeltaTime;
+        if (isFinished) return;
+        if (Vector3.Distance(transform.position, end.position) < finishRadius) 
+        {
+            isFinished = true;
+            return;
+        }
+        UpdateVelocity();
+        transform.position += _velocity * Time.fixedDeltaTime;
     }
 
-    /// <summary>
-    /// Checks if this person is in range of a 2d point
-    /// </summary>
-    /// <param name="point"> the pont ot check against </param>
-    /// <param name="range"> the range in both x and y axis from the point </param>
-    /// <returns> true if the person is in range of false if not</returns>
-    private bool IsInRangeOfPoint (Vector2 point, float range)
+    private void UpdateVelocity()
     {
-        Vector2 pos = transform.position;
-        return pos.x > point.x + range 
-               && pos.x < point.x - range 
-               && pos.y > point.y + range 
-               && pos.y < point.y - range;
+        _velocity = (end.position - transform.position).normalized * speed;
+        transform.up = _velocity;
     }
 }
