f1eeaa3500083040e222aff5518e85f07c2401b7
odast <oksteins@stud.ntnu.no>
2023-03-22 10:39:46 +0100

    FIxed compiler errors

--- Assets/Assets/UI/Score.cs
+++ Assets/Assets/UI/Score.cs
@@ -4,19 +4,19 @@ public class Score : MonoBehaviour
 {
     public int score;
     private TMP_Text display;
-    private void Start
+    private void Start()
     {
         score = 0;
         display = GetComponent<TMP_Text>();
         DisplayInt();
     }
 
-    private void DisplayInt
+    private void DisplayInt()
     {
         display.text = score;
     }
 
-    public void Update
+    public void Update()
     {
         DisplayInt();
     }
